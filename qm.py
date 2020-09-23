48053dc0cbb1749c7a5de67c302af8940a9cd6d2
Rishabh Ballal <rishabhballal@gmail.com>
2020-09-23 22:07:36 +0530

    generalised the code to different systems

--- qm.py
+++ qm.py
@@ -5,19 +5,18 @@ hbar = 1
 m = 1
 
 # the x axis range
-lim1 = -4
-lim2 = 4
+lim = 4
 step = 0.01
-x = np.arange(lim1, lim2, step)
+x = np.arange(-lim, lim, step)
 n = len(x)
 
 # the potential function
 V = x**2
 
-# represents the double derivative as a matrix using finite difference method
+# represents the double derivative operator as a matrix using the finite difference method
 mdd = 1/(step**2) * (np.diag(np.ones(n-1), k=-1)
-                     - 2 * np.diag(np.ones(n))
-                     + np.diag(np.ones(n-1), k=1))
+                            - 2 * np.diag(np.ones(n))
+                            + np.diag(np.ones(n-1), k=1))
 
 # sets up the hamiltonian as a matrix, and computes its eigenstuff
 H = -(hbar**2)/(2.0 * m) * mdd + np.diag(V)
@@ -29,11 +28,11 @@ fig, ax = plt.subplots(figsize=(8,6))
 ax.plot(x, V, color='k')
 print('Energies of the bound states:')
 for i in range(len(E)):
-	if(E[i] < max(V)):
-		print('E%s = %.2f' %(i, E[i]))
-		ax.plot(x, E[i] + 8*psi[i])
-	else:
-		break
+    if E[i] < max(V):
+        print('E%s = %.2f' %(i, E[i]))
+        ax.plot(x, E[i] + 8*psi[i])
+    else:
+        break
 ax.grid()
 ax.set_xlabel('Position: x')
 ax.set_ylabel('Energy: E')
